a89ab4b73730a9df9e8c9befab1fafebc212ef17
asif_np <asifnp1010@gmail.com>
2025-01-10 12:02:51 +0530

    included email env

--- api/api/settings.py
+++ api/api/settings.py
@@ -153,9 +153,15 @@ SECRET_KEY = config("SECRET_KEY")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587  
-EMAIL_USE_TLS = True  
-EMAIL_HOST_USER= "recruitdemo768@gmail.com"
-EMAIL_HOST_PASSWORD= "dtsm dpvb fved whxr"
+EMAIL_BACKEND = config("EMAIL_BACKEND")
+
+EMAIL_HOST = config("EMAIL_HOST") 
+
+EMAIL_PORT = config('EMAIL_PORT')
+ 
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
+EMAIL_HOST_USER= config('EMAIL_HOST_USER')
+
+EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD')
+
